d270afd0576c3e5a97fae39d20d43f43b14a0507
dbobra <bobra.darshan@gmail.com>
2019-06-05 15:05:04 +0530

    First Commit

---
+++ swaggerpetstore/src/main/java/com/dbobra/swagger/codegen/application/Application.java
@@ -0,0 +1,10 @@
+package com.dbobra.swagger.codegen.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
